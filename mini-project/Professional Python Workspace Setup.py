f58ea99279f1ca03ab66981f9f5c83b606ebb85b
Janusha-Weerasinghe <amjanushachamali@gmail.com>
2026-08-04 16:24:09 +0530

    Lesson 02- Visual Studio Code (VS Code)- Part 04 Note , Assignments , exercises and interview_questions

---
+++ mini-project/Professional Python Workspace Setup.py
@@ -0,0 +1,17 @@
+# ====================================================
+# Import Required Libraries
+# ====================================================
+
+import platform
+import sys
+
+print("=" * 50)
+print("Professional VS Code Workspace")
+print("=" * 50)
+
+print(f"Python Version : {sys.version.split()[0]}")
+print(f"Operating System : {platform.system()}")
+
+print("\nWorkspace Ready!")
+
+print("\nInstalled Successfully!")
